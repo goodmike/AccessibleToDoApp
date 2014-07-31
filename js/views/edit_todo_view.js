02b219a7ec0d7a915a9770f63fcf0a4c9357e270
Michael Harrison <mh@michaelharrison.ws>
2014-07-31 14:54:02 -0500

    Make to-do's editable by [Enter] key press, as well as double-click

--- js/views/edit_todo_view.js
+++ js/views/edit_todo_view.js
@@ -1,7 +1,13 @@
 Todos.EditTodoView = Ember.TextField.extend({
     didInsertElement: function() {
         this.$().focus();
-    }
+    },
+    keyPress: function(ev) { 
+      if (ev.keyCode === 13) {
+        this.$().blur();
+      }
+    },
+
 });
 
 Ember.Handlebars.helper('edit-todo', Todos.EditTodoView);
---
+++ js/views/todo_label_view.js
@@ -0,0 +1,11 @@
+Todos.TodoLabelView = Ember.View.extend({
+    keyPress: function(ev) { 
+      if (ev.keyCode === 13) {
+        this.get("controller").send("editTodo");
+      }
+    },
+    doubleClick: function() {
+      this.get("controller").send("editTodo");
+    },
+
+});
